c25395a3ad0f9a36388d836e51c0c91d2485fae4
Bravo <adevcalledbravo@gmail.com>
2020-12-29 18:41:42 +0100

    Initial Commit [Homepage, about, newspage done!]

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuButton = document.querySelector(".hamburger-icon");
+let closeButton = document.querySelector(".close-menu-icon");
+let mobileNavMenu = document.querySelector(".mobile-nav-menu");
+
+menuButton.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    mobileNavMenu.getElementsByClassName.height = "100%";
+}

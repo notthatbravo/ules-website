8cdee8a07ab9fa70785766aa962dd50f67530a1d
Bravo <adevcalledbravo@gmail.com>
2020-12-31 15:13:15 +0100

    Nav for mobile

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let closeButton = document.querySelector(".close-menu-icon");
 let mobileNavMenu = document.querySelector(".mobile-nav-menu");
 
 menuButton.addEventListener("click", toggleNav);
+closeButton.addEventListener("click", hideNav);
 
 function toggleNav() {
-    mobileNavMenu.getElementsByClassName.height = "100%";
+    mobileNavMenu.style.height = "20em";
+    menuButton.style.display = "none";
+    closeButton.style.display = "block";
+}
+
+function hideNav() {
+    mobileNavMenu.style.height = "0";
+    menuButton.style.display = "block";
+    closeButton.style.display = "none";
 }
